c92a7245cfe6eb628e088ba4e79fae5638d0dace
Quentiiiiiin <quent.lefeuvre@gmail.com>
2022-05-23 16:45:32 +0200

    dénut info

---
+++ src/main/java/fr/insalyon/p2i2/application/Information.java
@@ -0,0 +1,18 @@
+package fr.insalyon.p2i2.application;
+
+import javax.swing.JLabel;
+
+public class Information extends JLabel {
+    
+    double info  = 0; 
+    JLabel rendu;
+        
+    public Information(String capteur){
+
+        double info = 0; //Changer 0 et mettre la valeur du capteur actuek à récupérer avec la base de donnée
+            
+        rendu = new JLabel (capteur + " : " + info);
+
+    }
+        
+}
