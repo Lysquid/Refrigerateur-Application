9d655124de4ca3cc7856645b2c4bd9971d112563
Lysquid <romainbenoit21560@gmail.com>
2022-05-24 07:39:14 +0200

    info

--- src/main/java/fr/insalyon/p2i2/application/Information.java
+++ src/main/java/fr/insalyon/p2i2/application/Information.java
@@ -4,17 +4,24 @@ import javax.swing.JLabel;
 
 public class Information extends Compo {
 
-    String capteur;
-    JLabel info = new JLabel();
+    JLabel labelCapteur = new JLabel();
+    JLabel labelValeur = new JLabel();
+    String unite;
 
-    public Information(String capt) {
-        this.capteur = capt;
-        info = new JLabel();
-        this.add(info);
+    public Information(String capteur, String unite) {
+
+        labelCapteur = new JLabel(capteur);
+        labelCapteur.setFont(font);
+        labelValeur = new JLabel();
+        labelValeur.setFont(font);
+        this.unite = unite;
+        maj(0);
+        this.add(labelCapteur);
+        this.add(labelValeur);
     }
 
-    public void maj(double newValeur) {
-        info.setText(capteur + " : " + newValeur);
+    public void maj(double valeur) {
+        labelValeur.setText(String.valueOf(valeur) + " " + unite);
     }
 
 }
